e549a0e4654bc3e600099de669100d39bdaf40c9
Mannorosch <sebastian.roth@tuhh.de>
2021-05-05 17:36:12 +0200

    SalesAnalysis

--- sales_analysis.R
+++ sales_analysis.R
@@ -56,28 +56,87 @@ bike_orderlines_joined_tbl %>% glimpse()
 
 
 # 5.0 Wrangling Data ----
-
+bike_orderlines_wrangled_tbl <- bike_orderlines_joined_tbl %>%
+  
+  # 5.1 Separate category name
+  separate(col    = location,
+           into   = c("city", "state"),
+           sep    = ",") %>%
+  
+  # 5.2 Add the total price (price * quantity) 
+  # Add a column to a tibble that uses a formula-style calculation of other columns
+  mutate(total.price = price * quantity) %>%
+  
+  # 5.3 Optional: Reorganize. Using select to grab or remove unnecessary columns
+  # 5.3.1 by exact column name
+  select(-...1, -gender) %>%
+  
+  # 5.3.2 by a pattern
+  # You can use the select_helpers to define patterns. 
+  # Type ?ends_with and click on Select helpers in the documentation
+  select(-ends_with(".id")) %>%
+  
+  # 5.3.3 Actually we need the column "order.id". Let's bind it back to the data
+  bind_cols(bike_orderlines_joined_tbl %>% select(order.id)) %>% 
+  
+  # 5.3.4 You can reorder the data by selecting the columns in your desired order.
+  # You can use select_helpers like contains() or everything()
+  select(order.id, contains("order"), contains("model"), contains("category"),
+         price, quantity, total.price,
+         everything()) %>%
+  
+  # 5.4 Rename columns because we actually wanted underscores instead of the dots
+  # (one at the time vs. multiple at once)
+  rename(bikeshop = name) %>%
+  set_names(names(.) %>% str_replace_all("\\.", "_"))
+  
 
 # 6.0 Business Insights ----
-# 6.1 Sales by Year ----
+# 6.1 Sales by Year state ----
 
 # Step 1 - Manipulate
+sales_by_year_cat_1_tbl <- bike_orderlines_wrangled_tbl %>%
+  
+  # Select columns and add a year
+  select(order_date, total_price, state) %>%
+  mutate(year = year(order_date)) %>%
+  
+  # Group by and summarize year and main catgegory
+  group_by(year, state) %>%
+  summarise(sales = sum(total_price)) %>%
+  ungroup() %>%
+  
+  # Format $ Text
+  mutate(sales_text = scales::dollar(sales, big.mark = ".", 
+                                     decimal.mark = ",", 
+                                     prefix = "", 
+                                     suffix = " €"))
 
+sales_by_year_cat_1_tbl  
 # Step 2 - Visualize
+```{r, fig.width = 15, fig.height=12}
+sales_by_year_cat_1_tbl %>%
 
+  # Set up x, y, fill
+  ggplot(aes(x = year, y = sales, fill = state)) +
+  
+  # Geometries
+  geom_col() + # Run up to here to get a stacked bar plot
+  geom_smooth(method = "lm", se = FALSE) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  
+  # Facet
+  facet_wrap(~ state) +
+  
+  # Formatting
+  scale_y_continuous(labels = scales::dollar_format(big.mark = ".", 
+                                                    decimal.mark = ",", 
+                                                    prefix = "", 
+                                                    suffix = " €")) +
+  labs(
+    title = "Bike sell revenue",
+    subtitle = "By year and state",
+    fill = "Main category" # Changes the legend name
+  )
+```
 
-# 6.2 Sales by Year and Category 2 ----
-
-# Step 1 - Manipulate
-
-# Step 2 - Visualize
-
-
-
-# 7.0 Writing Files ----
-
-# 7.1 Excel ----
-
-# 7.2 CSV ----
-
-# 7.3 RDS ----
